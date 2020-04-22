abf2ab1b501a5d9ef8a4293be5e5bdec96aee9d4
Shau Yin Luk <ivan.luk@icloud.com>
2020-04-21 19:15:04 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,59 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get "/" do
+  end
+
+  # Index
+  get "/articles/?" do
+    @articles = Article.all
+    erb :index
+  end
+
+  # New
+  get "/articles/new" do
+    erb :new
+  end
+
+  # Show
+  get "/articles/:id" do
+    find_article
+    erb :show
+  end
+  
+  # Create
+  post "/articles" do
+    # article = Article.new(title: params[:title], content: params[:content])
+    article = Article.create(title: params[:title], content: params[:content])
+    # article.save
+    redirect "/articles/#{article.id}"
+  end
+
+  # Edit
+  get "/articles/:id/edit" do
+    find_article
+    erb :edit
+  end
+  
+  # Update
+  patch "/articles/:id" do
+    find_article
+    @article.update(title: params[:title], content: params[:content])
+    redirect "/articles/#{@article.id}"
+  end
+
+  # Delete
+
+  delete "/articles/:id" do
+    find_article
+    @article.destroy
+    redirect "/articles"
+  end
+  
+  
+  private
+
+  def find_article
+    @article = Article.find(params[:id])
   end
 end
